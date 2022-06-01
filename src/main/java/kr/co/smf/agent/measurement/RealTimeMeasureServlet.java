902c7e70f05f2269470efc3c61719b7fc5acc224
KimMinJae <alswo9588@gmail.com>
2022-06-01 20:44:07 +0900

    AgentUtil.java / RealTimeMeasureServlet.java Update

--- src/main/java/kr/co/smf/agent/measurement/RealTimeMeasureServlet.java
+++ src/main/java/kr/co/smf/agent/measurement/RealTimeMeasureServlet.java
@@ -1,15 +1,30 @@
 package kr.co.smf.agent.measurement;
 
+import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.smf.agent.beans.Measurement;
+import kr.co.smf.agent.util.AgentUtil;
+
 public class RealTimeMeasureServlet extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	ServletContext servletContext = this.getServletContext();
+    	
+    	String directoryPath = servletContext.getRealPath(File.separator + "WEB-INF" + File.separator +  "measurement.properties");
+    	
+    	AgentUtil agentUtil = new AgentUtil();
+    	agentUtil.setMeasurementPropertiesPath(directoryPath);
+    	
+    	Measurement measurement = agentUtil.selectGrowthMeasurementFile();
+    	
+    	request.setAttribute("measurement", measurement);
+    }
 }
--- src/main/java/kr/co/smf/agent/util/AgentUtil.java
+++ src/main/java/kr/co/smf/agent/util/AgentUtil.java
@@ -1,5 +1,10 @@
 package kr.co.smf.agent.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.Properties;
+
 import kr.co.smf.agent.beans.Agent;
 import kr.co.smf.agent.beans.Measurement;
 import kr.co.smf.agent.beans.Setting;
@@ -47,10 +52,77 @@ public class AgentUtil {
 	}
 	
 	public Measurement selectGrowthMeasurementFile() {
-		return null;
+		FileReader fileReader = null;
+		Measurement measurement = null;
+
+		try {
+			fileReader = new FileReader(measurementPropertiesPath);
+
+			Properties properties = new Properties();
+			properties.load(fileReader);
+
+			measurement = new Measurement();
+			measurement.setAgentIpAddress(properties.getProperty("agentIpAddress"));
+			measurement.setTemperature(Double.valueOf(properties.getProperty("temperature")));
+			measurement.setHumidity(Integer.valueOf(properties.getProperty("humidity")));
+			measurement.setCo2(Integer.valueOf(properties.getProperty("co2")));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fileReader != null) {
+					fileReader.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return measurement;
 	}
-	
+
 	public boolean updateGrowthMeasurementFile(Measurement measurement) {
-		return false;
+		FileReader fileReader = null;
+		FileOutputStream fileOutputStream = null;
+
+		try {
+			fileReader = new FileReader
+				("." + File.separator + "WebContent"  
+			         + File.separator + "WEB-INF"  
+			         + File.separator + "measurement.properties");
+
+			Properties properties = new Properties();
+			properties.load(fileReader);
+			
+			properties.setProperty("agentIpAddress", measurement.getAgentIpAddress());
+			properties.setProperty("temperature", String.valueOf(measurement.getTemperature()));
+			properties.setProperty("humidity", String.valueOf(measurement.getHumidity()));
+			properties.setProperty("co2", String.valueOf(measurement.getCo2()));
+			
+			fileOutputStream = new FileOutputStream
+					("." + File.separator + "WebContent"  
+			             + File.separator + "WEB-INF"  
+			             + File.separator + "measurement.properties");
+			
+			properties.store(fileOutputStream, "갱신");
+			fileOutputStream.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if (fileOutputStream != null) {
+					fileOutputStream.close();
+				}
+				if (fileReader != null) {
+					fileReader.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return true;
 	}
 }
