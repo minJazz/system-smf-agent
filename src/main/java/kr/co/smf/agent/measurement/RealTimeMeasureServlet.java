29cd53ea63981fe178999cc35fdd962af7e9fc79
sdm05 <sdm05100@naver.com>
2022-06-06 04:09:11 +0900

    RealTimeMeasureServlet update

--- src/main/java/kr/co/smf/agent/measurement/RealTimeMeasureServlet.java
+++ src/main/java/kr/co/smf/agent/measurement/RealTimeMeasureServlet.java
@@ -2,12 +2,15 @@ package kr.co.smf.agent.measurement;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.co.smf.agent.beans.Measurement;
 import kr.co.smf.agent.util.AgentUtil;
@@ -21,6 +24,14 @@ public class RealTimeMeasureServlet extends HttpServlet {
     	
     	Measurement measurement = agentUtil.selectGrowthMeasurementFile();
     	
-    	request.setAttribute("measurement", measurement);
+    	
+    	response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(measurement);
+		out.print(json);
+		out.flush();
     }
 }
